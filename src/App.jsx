9d993b6ca4cf7062f4e00c972cdf6f54290eac60
natnael-tilahun <natnaeltilahun97@gmail.com>
2023-09-14 23:55:18 +0300

    Add google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Hero from "./containers/HeroSection";
 import Header from "../src/components/Header";
@@ -11,8 +11,43 @@ import { Footer } from "./containers/FooterSection";
 import { Home } from "./pages/Home";
 import { Jobs } from "./pages/Jobs";
 import { Outlet } from "react-router-dom";
+import ReactGA from "react-ga4";
 
 function App() {
+  const TRACKING_ID = "G-V0ZL3SKR3Z";
+  ReactGA.initialize(TRACKING_ID);
+
+  ReactGA.event({
+    category: "Button",
+    action: "Click",
+    label: "Search",
+  });
+
+  ReactGA.event({
+    category: "Button",
+    action: "Click",
+    label: "Register",
+  });
+
+  ReactGA.event({
+    category: "Button",
+    action: "Click",
+    label: "Login",
+  });
+
+  ReactGA.event({
+    category: "Button",
+    action: "Click",
+    label: "Join Today",
+  });
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname + window.location.search,
+    });
+  }, []);
+
   return (
     <div className="App w-full m-0 box-border bg-thm_secondary_background">
       <Header />
