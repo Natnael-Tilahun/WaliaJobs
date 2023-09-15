3a0f665e71136cfd7724384a22db024699d52725
natnael-tilahun <natnaeltilahun97@gmail.com>
2023-09-15 11:37:13 +0300

    Change google analytics tracking id

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,49 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import Hero from "./containers/HeroSection";
-import Header from "../src/components/Header";
-import { JobCatagories } from "./containers/JobCatagoriesSection";
-import { Companies } from "./containers/CompaniesSection";
-import { JobsList } from "./containers/JobsListSection";
-import { BuildCvSection } from "./containers/BuildCvSection";
-import { SubscribeSection } from "./containers/SubscribeSection";
-import { Footer } from "./containers/FooterSection";
-import { Home } from "./pages/Home";
-import { Jobs } from "./pages/Jobs";
-import { Outlet } from "react-router-dom";
-import ReactGA from "react-ga4";
+import { useEffect, useState } from 'react';
+import './App.css';
+import Hero from './containers/HeroSection';
+import Header from '../src/components/Header';
+import { JobCatagories } from './containers/JobCatagoriesSection';
+import { Companies } from './containers/CompaniesSection';
+import { JobsList } from './containers/JobsListSection';
+import { BuildCvSection } from './containers/BuildCvSection';
+import { SubscribeSection } from './containers/SubscribeSection';
+import { Footer } from './containers/FooterSection';
+import { Home } from './pages/Home';
+import { Jobs } from './pages/Jobs';
+import { Outlet } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 
 function App() {
-  const TRACKING_ID = "G-V0ZL3SKR3Z";
+  const TRACKING_ID = 'G-QPPSENWWKM';
   ReactGA.initialize(TRACKING_ID);
 
   ReactGA.event({
-    category: "Button",
-    action: "Click",
-    label: "Search",
+    category: 'Button',
+    action: 'Click',
+    label: 'Search',
   });
 
   ReactGA.event({
-    category: "Button",
-    action: "Click",
-    label: "Register",
+    category: 'Button',
+    action: 'Click',
+    label: 'Register',
   });
 
   ReactGA.event({
-    category: "Button",
-    action: "Click",
-    label: "Login",
+    category: 'Button',
+    action: 'Click',
+    label: 'Login',
   });
 
   ReactGA.event({
-    category: "Button",
-    action: "Click",
-    label: "Join Today",
+    category: 'Button',
+    action: 'Click',
+    label: 'Join Today',
   });
 
   useEffect(() => {
     ReactGA.send({
-      hitType: "pageview",
+      hitType: 'pageview',
       page: window.location.pathname + window.location.search,
     });
   }, []);
